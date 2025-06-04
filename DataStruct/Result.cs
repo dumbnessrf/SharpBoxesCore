dbecf59634bce1e290693d02dc88d605b17b2746
zhenghc <zhenghc@daheng-imaging.com>
2025-06-04 10:14:02 +0800

    Add Result class and update ConfigFileHelper documentation
    - Introduced a new `Result` class in `Result.cs` to serve as a generic result wrapper with properties for success status, message, and code.
    - Added static methods for creating success and failure instances, including predefined failure scenarios.
    - Updated XML documentation for the `ConfigFileHelper` class to include a new `ExeFolder` member with a summary in Chinese.

---
+++ DataStruct/Result.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Xml.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using HalconDotNet;
+
+namespace SharpBoxesCore.DataStruct;
+
+/// <summary>
+/// 通用结果类
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// 获取或设置操作是否成功的标志
+    /// </summary>
+    public bool IsSuccess { get; set; }
+
+    public bool IsFail => !IsSuccess;
+
+    /// <summary>
+    /// 获取或设置操作结果的消息
+    /// </summary>
+    public string Message { get; set; }
+
+    /// <summary>
+    /// 获取或设置操作结果的代码
+    /// </summary>
+    public int Code { get; set; }
+
+    /// <summary>
+    /// 初始化 Result 类的新实例
+    /// </summary>
+    /// <param name="success">操作是否成功的标志</param>
+    /// <param name="message">操作结果的消息，默认为空字符串</param>
+    /// <param name="code">操作结果的代码，默认为 0</param>
+    public Result(bool success, string message = "", int code = 0)
+    {
+        IsSuccess = success;
+        Message = message;
+        Code = code;
+    }
+
+    /// <summary>
+    /// 创建一个表示操作成功的 Result 实例
+    /// </summary>
+    /// <param name="message">操作结果的消息，默认为空字符串</param>
+    /// <param name="code">操作结果的代码，默认为 0</param>
+    /// <returns>表示操作成功的 Result 实例</returns>
+    public static Result ReturnSuccess(string message = "", int code = 0)
+    {
+        return new Result(true);
+    }
+
+    /// <summary>
+    /// 创建一个表示操作失败的 Result 实例
+    /// </summary>
+    /// <param name="message">操作结果的消息，默认为空字符串</param>
+    /// <param name="code">操作结果的代码，默认为 0</param>
+    /// <returns>表示操作失败的 Result 实例</returns>
+    public static Result ReturnFail(string message = "", int code = 0)
+    {
+        return new Result(false, message, code);
+    }
+
+    /// <summary>
+    /// 表示操作成功状态的静态实例
+    /// </summary>
+    public static Result Success => new(true);
+
+    /// <summary>
+    /// 表示操作失败状态的静态实例
+    /// </summary>
+    public static Result Fail => new(false);
+
+    public static Result AuthorityFail => new(false, "权限不足", 403);
+
+    public static Result NotFoundFail => new(false, "未找到资源", 404);
+
+    public static Result ServerErrorFail => new(false, "服务器错误", 500);
+
+    public static Result ParameterErrorFail => new(false, "参数错误", 400);
+
+    public static Result NotInitializedFail => new(false, "未初始化", 500);
+
+    public static Result NotSupportedFail => new(false, "不支持", 500);
+}
